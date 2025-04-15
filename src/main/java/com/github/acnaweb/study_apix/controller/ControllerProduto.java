26d8eb0527b4a68a7a991bf9632a5952ae8443ec
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-14 21:39:24 -0300

    aula07/04

---
+++ src/main/java/com/github/acnaweb/study_apix/controller/ControllerProduto.java
@@ -0,0 +1,67 @@
+package com.github.acnaweb.study_apix.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.acnaweb.study_apix.dto.ProdutoRequestCreate;
+import com.github.acnaweb.study_apix.model.Produto;
+import com.github.acnaweb.study_apix.service.ProdutoService;
+
+@RestController
+@RequestMapping("produtos")
+public class ControllerProduto {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<Produto> 
+                create(@RequestBody ProdutoRequestCreate dto) {
+
+        Produto produto = produtoService.save(dto);
+
+        return ResponseEntity.status(201).body(produto);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> update() {
+        return ResponseEntity.status(200).body("Produto atualizado");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll() {
+        List<Produto> produtos = produtoService.findAll();        
+        return ResponseEntity.status(200).body(produtos);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id) {         
+        return produtoService.findById(id)
+            .map(p -> ResponseEntity.ok(p))
+            .orElse(ResponseEntity.notFound().build());        
+    }
+    
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        boolean result = produtoService.deleteById(id);
+
+        if (result) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }        
+    }
+
+}
---
+++ src/main/java/com/github/acnaweb/study_apix/service/ProdutoService.java
@@ -0,0 +1,43 @@
+package com.github.acnaweb.study_apix.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.acnaweb.study_apix.dto.ProdutoRequestCreate;
+import com.github.acnaweb.study_apix.model.Produto;
+import com.github.acnaweb.study_apix.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto save(ProdutoRequestCreate dto) {
+                
+        Produto produto = new Produto();
+        produto.setNome(dto.getNome());
+
+        return produtoRepository.save(produto);
+    }
+
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+    
+    public Optional<Produto> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public boolean deleteById(Long id){
+        if (produtoRepository.existsById(id)){
+            produtoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
